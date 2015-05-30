94a60df058d2fe39479d8ec6a88c094289a6caad
telcontarvi <telcontarvi@gmail.com>
2015-05-30 10:24:28 +0200

    Primera Pregunta

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
+var quizController =require("../controllers/quiz_controller")
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Quiz Tel' });
 });
+
+router.get("/quizes/question",quizController.question);
+router.get("/quizes/answer",quizController.answer);
 
 module.exports = router;
