ca58c5da33d95092ed3247f4449c2c4130092f6d
telcontarvi <telcontarvi@gmail.com>
2015-06-04 10:59:47 +0200

    Adaptacion DB Heroku

--- models/models.js
+++ models/models.js
@@ -1,8 +1,25 @@
 var path=require("path");
+
+var dbURL=process.env.DATABASE_URL||"sqlite://:@:/";
+var url=dbURL.match(/(.*)\:\/\/(.*?)\:(.*)@(.*)\:(.*)\/(.*)/);
+var DB_name= (url[6]||null);
+var user= (url[2]||null);
+var pwd= (url[3]||null);
+var protocol= (url[1]||null);
+var dialect= (url[1]||null);
+var port= (url[5]||null);
+var host= (url[4]||null);
+var storage=process.env.DATABASE_STORAGE;
+
 var Sequelize=require("sequelize");
 
-var sequelize=new Sequelize(null,null,null,
-					{dialect:"sqlite",storage:"quiz.sqlite"});
+var sequelize=new Sequelize(DB_name,user,pwd,
+					{	dialect:protocol,
+						protocol:protocol,
+						port:port,
+						host:host,					
+						storage:storage,
+						omitNull: true});
 
 var Quiz=sequelize.import(path.join(__dirname,"quiz"));
 exports.Quiz=Quiz;
