4dfade8b1bd1c9d2923d45013be512efbd69d0d3
telcontarvi <telcontarvi@gmail.com>
2015-06-04 10:13:19 +0200

    Base de Datos

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,14 +1,21 @@
+var models=require("../models/models.js");
 //GET quizes/question
 exports.question=function(req,res){
-	res.render("quizes/question",{pregunta:"Capital de Italia"});
+	models.Quiz.findAll().then(function(quiz){
+		res.render("quizes/question",{pregunta:quiz[0].pregunta});
+	});
+	
 };
 
 //GET quizes/answer
 exports.answer=function(req,res){
+	models.Quiz.findAll().then(function(quiz){
+		if(req.query.respuesta && req.query.respuesta.toUpperCase()===quiz[0].respuesta.toUpperCase()){
+			res.render("quizes/answer",{respuesta:"Correcto"});
+		}else{
+			res.render("quizes/answer",{respuesta:"Incorrecto"});
+		}
+	});	
 	//console.log("R: "+req.query.respuesta);
-	if(req.query.respuesta && req.query.respuesta.toUpperCase()==="ROMA"){
-		res.render("quizes/answer",{respuesta:"Correcto"});
-	}else{
-		res.render("quizes/answer",{respuesta:"Incorrecto"});
-	}	
+	
 };
