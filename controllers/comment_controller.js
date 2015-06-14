0e39f45188d60402635d9b97a7bdaa0e9311ab8e
telcontarvi <telcontarvi@gmail.com>
2015-06-14 22:07:24 +0200

    HTTPS

--- controllers/comment_controller.js
+++ controllers/comment_controller.js
@@ -1,5 +1,19 @@
 var models=require("../models/models.js");
 
+exports.load=function(req,res,next,commentId){
+	models.Comment.find({
+		where:{id: Number(commentId)}
+	}).then(function(comment){
+		if(comment){
+			req.comment=comment;
+			next();
+		}else{
+			next(new Error("No existe el commentID="+commentId));
+		}
+	}
+	).catch(function(error){next(error)});
+};
+
 exports.new=function(req,res){
 	console.log("Nuevo Comentario");
 	res.render("comments/new.ejs",{quiz:req.quiz,errors:[]})	;
@@ -22,4 +36,12 @@ exports.create=function(req,res){
 			}
 		}
 	).catch(function(error){next(error)});
-}
+};
+
+exports.publish=function(req,res){
+	req.comment.publicado=true;
+	req.comment.save(
+		{fields:["publicado"]})
+	.then(function(){res.redirect("/quizes/"+req.params.quizId);})
+	.catch(function(error){next(error)});
+};
--- models/comment.js
+++ models/comment.js
@@ -1,9 +1,13 @@
 module.exports=function(sequelize,DataTypes){
 	return sequelize.define(
 		"Comment",
-		{texto:{
+		{	texto:{
 				type: DataTypes.STRING,
 				validate:{notEmpty:{msg:"-> Falta Comentario"}}
+			},
+			publicado:{
+				type: DataTypes.BOOLEAN,
+				defaultValue: false
 			}
 		}		
 	);
